0b386c5f883a379933b49147dae258667a98170d
Daniele Zavaglia <cdzavagliadaniele@gmail.com>
2022-04-22 14:46:47 +0200

    Ho pultio il corpo del programma, creando una funzione adibita alla restituzione dei valori all'utente

--- ConvBidirezionaleCFeKmMi.ts
+++ ConvBidirezionaleCFeKmMi.ts
@@ -1,60 +1,69 @@
 function convCelFar(temperatura:number):number{
     meas = (inp*9/5)+32
-    return meas 
+    return meas; 
 }
 function convFarCel(temperatura:number):number{
     meas = (inp-32) * 5/9
-    return meas 
+    return meas; 
 }
-function convKmMi(Distanza:number):number{
+function convKmMi(distanza:number):number{
     meas = inp / 1.6
-    return meas 
+    return meas; 
 }
-function convMiKm(Distanza:number):number{
+function convMiKm(distanza:number):number{
     meas = inp * 1.6
-    return meas 
+    return meas;
 }
+function stampTempConvertita(valoreInp:number,unitaDiMisura:string):void{
+    if (unita=="c"||unita=="C"){
+    console.log("Hai inserito "+inp+"°"+unita.toUpperCase());
+    console.log("Che equivale a "+meas+"°"+"F");
+    }
+    if (unita=="f"||unita=="F"){
+        console.log("Hai inserito "+inp+"°"+unita.toUpperCase());
+        console.log("Che equivale a "+meas+"°"+"C");
+    }
+    if (unita=="km" || unita=="KM"||unita=="Km"){
+    console.log("Hai inserito "+inp+unita.toUpperCase());
+    console.log("Che equivale a "+meas+"°"+"MI");
+    }
+    if(unita=="mi"||unita=="MI"||unita=="Mi"){
+    console.log("Hai inserito "+inp+unita.toUpperCase());
+    console.log("Che equivale a "+meas+"°"+"KM");
+    }
+    
+}   
+    
+    
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let cho:number=prompt("Inserisci '1' per convertitore temperatura o '2' per convertire distanza")*1;
 let inp:number=prompt("Inserisci il numero da convertire")*1;
 let unita:string=prompt("Patate? Pere? Mele? specifica l'unita' di misura del precedente numero inserito");
-
-
 let meas:number;
-if (cho==1){
-    if(unita=="c" || unita=="C"){
+
+if (cho > 2 || cho < 1){
+    console.log("Non hai inserito un comando valido, per favore riavvia il programma.");
+}
+if (cho == 1){
+    if (unita=="c"||unita=="C"){
     convCelFar(inp);
-    console.log("Hai inserito"+inp +" °C");
-    console.log("Che equivale a "+meas +" °F" );
+    stampTempConvertita(inp,unita);
     }
-    else{
-        console.log("Unita' di misura non corretta");
+    if (unita=="f"||unita=="F"){
+    convFarCel(inp);
+    stampTempConvertita(inp,unita);
     }
-    if(unita=="f"||unita=="F"){
-        convFarCel(inp);
-        console.log("Hai inserito "+inp +" °F");
-        console.log("Che equivale a "+meas +" °C" );
-    }
-    else{
-        console.log("Unita' di misura non corretta");
-    }
-    }
-if (cho==2){
+}
+if (cho ==2){
     if(unita=="km" || unita=="KM"||unita=="Km"){
     convKmMi(inp);
-    console.log("Hai inserito "+inp +" Km");
-    console.log("Che equivale a "+meas +" Mi");
+    stampTempConvertita(inp,unita);
     }
     
     if(unita=="mi"||unita=="MI"||unita=="Mi"){
-        convMiKm(inp);
-        console.log("Hai inserito "+inp +" Mi");
-        console.log("Che equivale a "+meas +" Km" );
+    convMiKm(inp);
+    stampTempConvertita(inp,unita);    
     }
-    
-}
-if (cho >2 || cho < 1){
-    console.log("Riavvia il programma e scegli SOLO tra '1' e '2'");
 }
