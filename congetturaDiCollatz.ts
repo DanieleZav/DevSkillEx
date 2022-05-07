9fb040787babab9df9fef9621dbf7a3ea96668f9
Daniele Zavaglia <cdzavagliadaniele@gmail.com>
2022-05-07 20:24:26 +0200

    Congettura di Collatz una func di calcolo e una di swapping

---
+++ congetturaDiCollatz.ts
@@ -0,0 +1,33 @@
+const operation = (oneNumber:number) => {
+    const oneArray:number[] = [oneNumber]
+    while (oneNumber > 1){ 
+        (oneNumber % 2 === 0) ? oneNumber = oneNumber / 2 : oneNumber = (oneNumber * 3) + 1;
+        oneArray.push(oneNumber);
+    }
+    //console.log("Func", oneArray);
+    return oneArray;
+}
+const swapNumber = (oneNumber:number, anotherNumber:number) => {
+    oneNumber = oneNumber + anotherNumber;
+    anotherNumber = oneNumber - anotherNumber;
+    oneNumber = oneNumber - anotherNumber;
+    return [oneNumber, anotherNumber]
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+let i:number = +prompt("Numero 'i'");
+let j:number = +prompt("Numero 'j'");
+let maxLengthCycle:number[] = []
+
+if (i > j){
+    const [firstNumber, secondNumber] = swapNumber(i,j);    //destrutturazione
+    i = firstNumber;
+    j = secondNumber;
+}
+for (let x = i; x <= j; x++){
+    const lengthCycle = operation(x)
+    if (lengthCycle.length > maxLengthCycle.length){
+        maxLengthCycle = lengthCycle
+    }
+}
+console.log(i, j, maxLengthCycle.length)
